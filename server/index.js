ff74f5cac33f6cab719d970b4e20566f38101881
Victor Ngo <psychosete@gmail.com>
2017-06-22 20:19:27 -0700

    (fix) Make express attempt to connect to port number: process.env.port OR 3000 from 3000.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,8 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
  
-<<<<<<< 87d5de3dcd2073fb8f5ff5b12575b5acb255ae52
 var port = process.env.PORT || 3000;
-=======
-var port = 3000;
->>>>>>> Add basic express server.
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })

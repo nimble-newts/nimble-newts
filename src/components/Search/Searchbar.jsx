badcf7398955517ed27a57ffefa02df893013a0a
gbaik <gideonbaik@gmail.com>
2017-06-25 16:06:45 -0700

    Add function to receive search bar text on click

--- src/components/Search/Searchbar.jsx
+++ src/components/Search/Searchbar.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
-const SearchBar = ({}) => (
-  <form className="Search-bar">
-    <input type="text"></input>
-    <input type="submit" value="Search"></input>
-  </form>
-);
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: ''
+    };
+    
+    this.handleSearchText = this.handleSearchText.bind(this);    
+    this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+  }
+
+  handleSearchText(event) {
+    this.setState({
+      searchText: event.target.value
+    });
+  }
+
+  handleSearchSubmit() {
+    console.log(this.state.searchText);
+  }
+
+  render() {
+    return (
+      <div className = "Search-bar">
+        <input type="text" onChange={this.handleSearchText}></input>
+        <input type="submit" value="Search" onClick={this.handleSearchSubmit}></input>
+      </div>
+    );
+  }
+}
 
 export default SearchBar;

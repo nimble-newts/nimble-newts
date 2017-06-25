88827561f19e1f49ec6247fe202a51aa3495c1b3
gbaik <gideonbaik@gmail.com>
2017-06-24 19:21:40 -0700

    Add and fix index.html to properly render links
    * Add index.html
    
    * Fix index.html page to render correct links

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
+import { browserHistory } from 'react-router';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { Login } from './components/Login/index.jsx';
@@ -9,13 +10,21 @@ import { Profile } from './components/Profile/index.jsx';
 // let store = createStore(/*reducer*/);
 
 const Routes = ({ store }) => (
-  <Provider store={store}> 
   <Router history={browserHistory}>
+    <div>
+      <ul>
+        <li><Link to="/(:filter)">Home</Link></li>
+        <li><Link to="/search">Search</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+      </ul>
+
       <Route path="/(:filter)" component={Login} />
       <Route path="/search" component={Search} />
       <Route path="/profile" component={Profile} />
+    </div>
   </Router>
-  </Provider>
 );
 
 export default Routes;
+// <Provider store={store}> 
+// </Provider>

85edb8ccd476bda02738cc4764513c33cef4c0f7
Jennifer Wang <jw775@cornell.edu>
2017-06-25 20:32:07 -0700

    add nav buttons between search and profile

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import User from './User.jsx';
 import Friends from './Friends.jsx';
 import Suggestions from './Suggestions.jsx';
 
-const Profile = ({}) => (   
-  <div className="Profile">
-    <User />
-    <Friends />
-    <Suggestions />
-  </div>
-);
+class Profile extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleNav() {
+    document.location.href = '/search';
+  }
+
+  render() {
+    return (  
+      <div className="Profile">
+        <User />
+        <Friends />
+        <Suggestions />
+        <input type="submit" onClick={this.handleNav} value="Back to Search"></input>
+      </div>
+    );
+  }
+}
 
 export default Profile;

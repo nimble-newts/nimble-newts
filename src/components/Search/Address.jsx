fa56db4c83ee8b7e1fc936c8527a3ce49690a754
Jennifer Wang <jw775@cornell.edu>
2017-06-25 20:46:39 -0700

    implement add address field functionality

---
+++ src/components/Search/Address.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Address = ({}) => (
+  <div className="Address">
+    <input type="text"></input>
+  </div>
+);
+
+export default Address;
--- src/components/Search/Addresses.jsx
+++ src/components/Search/Addresses.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Address from './Address.jsx';
 
-const Addresses = ({}) => (
-  <div className="Addresses">
-    <input type="text"></input>
-    <input type="text"></input>
-    <input type="submit" value="Add Address"></input>
-  </div>
-);
+class Addresses extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { //refactor with redux
+      'number': 1
+    };
+
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd() {
+    this.setState(prevState => {
+      return { 'number': prevState.number + 1 }
+    });
+  }
+
+  render() {
+    let addresses = [];
+    for (let i = 0; i < this.state.number; i++) {
+      addresses.push(<Address key={i}/>);
+    }
+
+    return (
+      <div className="Addresses">
+        {addresses}
+        <input type="submit" onClick={this.handleAdd} value="Add Address"></input>
+      </div>
+    );
+  }
+}
 
 export default Addresses;

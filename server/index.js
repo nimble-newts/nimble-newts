4673d3b0aae5c1efc5376ea78cf52af649d44190
Victor Ngo <psychosete@gmail.com>
2017-06-27 15:07:58 -0700

    (test/fix) Changed server index and index html to refer to env vars

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ var express = require('express');
 var User = require('../database/db.js');
 var path = require('path');
 var request = require('request');
+var token = process.env.YELP_ACCESS_TOKEN;
+console.log('Uh oh, heres your token: ', token);
+console.log('HEres your port #', process.env.port);
+console.log('Heres the environmental vars', process.env);
 
 var app = express();
 var port = process.env.PORT || 3000;

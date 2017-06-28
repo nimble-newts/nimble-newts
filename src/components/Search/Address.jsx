27050a0c06dc9814da46c6ba6d3a6ae6fd30d6df
Jennifer Wang <jw775@cornell.edu>
2017-06-27 17:39:50 -0700

    Implement autocomplete for friends

--- src/components/Search/Address.jsx
+++ src/components/Search/Address.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
 
 class Address extends Component {
   constructor(props) {
@@ -8,9 +9,42 @@ class Address extends Component {
       address: ''
     };
 
+    this.friendsSource = [];
     this.handleAdd = this.handleAdd.bind(this);
     this.handleCancelAdd = this.handleCancelAdd.bind(this);
     this.handleSave = this.handleSave.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  populateSource(res) {
+    let friendsArr = [];
+    res.forEach(friend => {
+      let friendObj = {};
+      friendObj.label = friend.name;
+      friendObj.value = friend.address;
+      friendsArr.push(friendObj);
+    });
+    this.friendsSource = friendsArr;
+  }
+
+  componentDidMount() {
+    FB.api('/me', res => {
+      let getOptions = {
+        method: 'post',
+        body: JSON.stringify({
+          userID: res.id,
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+
+      fetch('/friends', getOptions).then(res => {
+        return res.json();
+      }).then(res => {
+        this.populateSource(res);
+      });
+    });
   }
 
   componentWillReceiveProps(newProps) {
@@ -48,15 +82,28 @@ class Address extends Component {
       };
 
       fetch('/save', saveOptions).then(res => {
-        this.setState({ adding: false });
+        return res.json();
+      }).then(res => {
+        this.populateSource(res);
+        this.setState({ 
+          adding: false,
+        });
       });
     });
+  }
+
+  handleChange(e) {
+    const el = findDOMNode(this.refs.address);
+    $(el).autocomplete({
+      source: this.friendsSource
+    });
+    this.setState({ address: e.target.value });
   }
 
   render() {
     return (
       <div className="Address">
-        <input type="text" value={this.state.address} onChange={(e) => this.setState({ address: e.target.value })} required></input>
+        <input type="text" value={this.state.address} onChange={this.handleChange} ref="address" required></input>
         {this.state.adding === false ? (
           <input type="submit" value="Add Friend" onClick={this.handleAdd}></input>
         ) : (

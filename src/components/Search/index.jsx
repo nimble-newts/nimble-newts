378c96494fbdf33759d650856f6c8970c2ea328b
Jennifer Wang <jw775@cornell.edu>
2017-06-29 14:51:21 -0700

    fix index and address components

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -67,7 +67,7 @@ class Search extends Component {
 
   grabAddresses(e) {
     let currentAddresses = [];
-    let addressesDiv = e.target.parentNode.parentNode.children[4].children;
+    let addressesDiv = e.target.parentNode.parentNode.parentNode.children[1].children[0].children;
     for (let i = 0; i < addressesDiv.length - 1; i++) {
       currentAddresses.push(addressesDiv[i].firstChild.firstChild.firstChild.value);
     }

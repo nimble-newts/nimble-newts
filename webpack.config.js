97c7356af9e7f655d3f775a8df38f96f828dab87
gbaik <gideonbaik@gmail.com>
2017-07-02 13:45:56 -0700

    Compile Semantic with Webpack
    * Fix Travis
    
    * Fix travis
    
    * Add Istanbul test coverage
    
    * Cleanup code with eslint
    
    * Update styling for login page
    
    * Update webpack config to compile semantic files
    
    * Fix index.html
    
    * Fix commit
    
    * Update compiled semantic name
    
    * Refactor webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,48 @@
 var path = require('path');
 var SRC_DIR = path.join(__dirname, '/src');
 var DIST_DIR = path.join(__dirname, '/dist');
+var SEMANTIC_DIR = path.join(__dirname, '/semantic/dist');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: {
+    semantic: [`${SEMANTIC_DIR}/semantic.min.css`, `${SEMANTIC_DIR}/semantic.min.js`],
+    bundle: `${SRC_DIR}/index.jsx`
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: DIST_DIR
   },
   module: {
     rules: [
+      {
+        loader: 'style-loader!css-loader',
+        include: SEMANTIC_DIR,
+        test: /\.css$/
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        loader: 'url-loader',
+        include: SEMANTIC_DIR,
+        options: {
+          limit: 10240,
+          absolute: true,
+          name: 'images/[path][name]-[hash:7].[ext]'
+        }
+      }, 
+      {
+        test: /\.(woff|woff2|ttf|svg|eot)$/,
+        include: SEMANTIC_DIR,
+        loader: 'url-loader',
+        options: {
+          limit: 10240,
+          name: 'fonts/[name]-[hash:7].[ext]'
+        }
+      },
+      {
+        loader: 'babel-loader',      
+        include: SEMANTIC_DIR,
+        test: /\.js?/,
+      },
       {
         loader: 'babel-loader',      
         include: SRC_DIR,
@@ -20,4 +53,4 @@ module.exports = {
       }
     ]
   }
-};
+}

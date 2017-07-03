71a03d13e84f763864566be0de15a064fb66b7e2
Jennifer Wang <jw775@cornell.edu>
2017-07-02 21:55:59 -0700

    remove unnecessary redux references

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { browserHistory } from 'react-router';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import Login from './components/Login/index.jsx';
 import Search from './components/Search/index.jsx';
 import Profile from './components/Profile/index.jsx';
 import NewUser from './components/NewUser/index.jsx';
 
-const Routes = ({ store }) => (
+const Routes = () => (
   <BrowserRouter>
     <div>
       <Route exact path="/" component={Login} />

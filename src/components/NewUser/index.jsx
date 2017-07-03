7938690118902209b73b65187d4ae6af028b4532
Jennifer Wang <jw775@cornell.edu>
2017-07-03 12:40:29 -0700

    Update new user page style
    * fix bug on add addresses with autocomplete
    
    * fix index and address components
    
    * restyle search page
    
    * restyle search page
    
    * implement autocomplete with semantic ui
    
    * test accordion
    
    * reformat add friend fields
    
    * add semantic folder
    
    * restyle user section of profile
    
    * restyle saved sections of profile
    
    * fix index and address components
    
    * restyle search page
    
    * restyle search page
    
    * implement autocomplete with semantic ui
    
    * test accordion
    
    * reformat add friend fields
    
    * restyle user section of profile
    
    * restyle saved sections of profile
    
    * satisfy linter
    
    * restyle new user page

--- src/components/NewUser/index.jsx
+++ src/components/NewUser/index.jsx
@@ -14,7 +14,29 @@ class NewUser extends Component {
   render() {
     return (  
       <div className="new-user">
-        <SimpleSlider changeToSearchOnClick={this.handleNav} />
+        <div className="ui centered grid">
+          <div className="twelve wide column">
+            <div className="ui raised very padded segment user">
+              <div className="ui centered grid">
+                <div className="row">
+                  <h1 className="ui header welcome">
+                    Welcome to Pinpoint!
+                    <div className="sub header">Let's get you pinning...</div>
+                  </h1>
+                </div>
+                <SimpleSlider />
+                <div className="row">
+                  <div className="right floated right aligned five wide column">
+                    <button className="ui secondary labeled icon button" onClick={this.handleNav}>
+                      <i className="arrow right icon"></i>
+                      Start Searching
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }

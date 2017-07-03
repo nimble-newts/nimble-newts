d02292bbf90e835dbf8036226edbdb45220ab994
gbaik <gideonbaik@gmail.com>
2017-07-03 00:27:58 -0700

    Fix database_spec

--- test/database_spec.js
+++ test/database_spec.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var database = require('../database/db.js');
+var User = require('../database/db.js');
 var chai = require('chai');
 var expect = chai.expect;
 
@@ -19,16 +19,16 @@ describe('MongoDB Test:', function() {
     });
   });
 
-  describe('Check if database correctly', function () {
+  describe('Check if database', function () {
 
     it('saves new user information', function(done) {
-      var userInfo = database({
-        name: 'Bleh', 
+      var userInfo = User({
+        name: 'Gideon', 
         defaultAddress: '944 Market St, San Francisco, CA 94102',
         friends: [
-          {name: 'Jennifer', address: 'Chinatown'}, 
-          {name: 'Victor', address: 'Daly City'}, 
-          {name: 'Patrick', address: 'Bikini Bottom'}
+          {name: 'Me', address: '944 Market St, San Francisco, CA 94102'}, 
+          {name: 'Myself', address: '944 Market St, San Francisco, CA 94102'}, 
+          {name: 'I', address: '944 Market St, San Francisco, CA 94102'}
         ],
         suggestions: [
           {name: 'Philz Coffee', address: '201 Berry St, San Francisco, CA 94158'}, 
@@ -41,40 +41,14 @@ describe('MongoDB Test:', function() {
     });
 
     it('retrieves new user information', function(done) {
-      var defaultAddress = {defaultAddress: '944 Market St, San Francisco, CA 94102'};
-      
-      var friends = 
-        {friends: [
-          {name: 'Jennifer', address: 'Chinatown'}, 
-          {name: 'Victor', address: 'Daly City'}, 
-          {name: 'Patrick', address: 'Bikini Bottom'}
-        ]};
-      var suggestions = 
-        {suggestions: [
-          {name: 'Philz Coffee', address: '201 Berry St, San Francisco, CA 94158'}, 
-          {name: 'Library', address: '100 Larkin St. San Francisco, 94102'}, 
-          {name: 'Krutsy Krab', address: '35 Powell St, San Francisco, CA 94102'}
-        ]};
-      
-      database.find(defaultAddress, function (err, defaultAddress) {
+      User.findOne({'name': 'Gideon'}, function (err, results) {
         if(err) {throw err;}
-        if(defaultAddress.length === 0) {throw new Error('No default_address found!');}
+        console.log('hello', results);
+        expect(results.defaultAddress).to.not.be.empty;
+        expect(results.friends).to.not.be.empty;
+        expect(results.suggestions).to.not.be.empty;        
         done();
       });
-
-      database.find(friends, function (err, friends) {
-        if(err) {throw err;}
-        if(friends.length === 0) {throw new Error('No friends found!');}
-        done();
-      });
-
-      database.find(suggestions, function (err, suggestions) {
-        console.log(`Suggestions: ${suggestions}`);
-        if(err) {throw err;}
-        if(suggestions.length === 0) {throw new Error('No saved suggestions found!');}
-        done();
-      });
-
     });
   });
 
